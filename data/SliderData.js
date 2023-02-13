a9827fe7a473ce179dea97d8803e0435b8d0f71d
Ashima1503 <78199579+Ashima1503@users.noreply.github.com>
2023-02-13 23:44:24 +0530

    Backend Real Estate Project

---
+++ data/SliderData.js
@@ -0,0 +1,30 @@
+import ImageOne from "../images/house1.jpg";
+import ImageTwo from "../images/house2.jpg";
+import ImageThree from "../images/house3.jpg";
+
+export const SliderData = [
+  {
+    title: "Find your Home",
+    price: "1cr",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "Home",
+  },
+  {
+    title: "Find your home",
+    price: "80lakhs",
+    path: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "Home",
+  },
+  {
+    title: "Find your home",
+    price: "85lakhs",
+    path: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "Home",
+  },
+];
